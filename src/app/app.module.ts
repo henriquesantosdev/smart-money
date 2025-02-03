170c6252befc8c5ac1b4d1c929a1bafd8c201588
henriquesantosdev <henriquesantos2277@gmail.com>
2025-02-03 14:07:44 -0300

    Configuring aplication middleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/users/users.module';
 import { BankAccountsModule } from 'src/bank-accounts/bank-accounts.module';
+import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 
 @Module({
   imports: [UserModule, BankAccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
