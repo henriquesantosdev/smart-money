cf38c5bba48f6f75e735a945bd5141737de5478e
henriquesantosdev <henriquesantos2277@gmail.com>
2025-02-06 19:59:00 -0300

    Removed loggerMiddleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/users/users.module';
 import { BankAccountsModule } from 'src/bank-accounts/bank-accounts.module';
-import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 import { AuthModule } from 'src/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -17,11 +11,4 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
