13811b237f9b3c365090bc80fa719312e7d25651
henriquesantosdev <henriquesantos2277@gmail.com>
2025-02-15 23:16:34 -0300

    bank_account's CRUD finished

--- src/bank-accounts/bank-accounts.controller.ts
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -1,12 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { BankAccountsService } from './bank-accounts.service';
 import { TokenPayloadParam } from 'src/auth/param/token-payload.param';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 
 @Controller('bank-account')
 export class BankAccountController {
   constructor(private readonly bankAccountsService: BankAccountsService) {}
+
+  @Get()
+  getBankAccounts(@TokenPayloadParam() tokenPayloadDto: TokenPayloadDto) {
+    return this.bankAccountsService.getBankAccounts(tokenPayloadDto);
+  }
 
   @Post()
   createBankAccount(
@@ -17,5 +31,23 @@ export class BankAccountController {
       createBankAccountDto,
       tokenPayloadDto,
     );
+  }
+
+  @Patch(':id')
+  updateBankAccount(
+    @Param('id') id: string,
+    @Body() updateBankAccountDto: UpdateBankAccountDto,
+    @TokenPayloadParam() tokenPayloadDto: TokenPayloadDto,
+  ) {
+    return this.bankAccountsService.updateBankAccount(
+      id,
+      updateBankAccountDto,
+      tokenPayloadDto,
+    );
+  }
+
+  @Delete(':id')
+  deleteBankAccount(@Param('id') id: string) {
+    return this.bankAccountsService.deleteBankAccount(id);
   }
 }
--- src/bank-accounts/bank-accounts.service.ts
+++ src/bank-accounts/bank-accounts.service.ts
@@ -2,6 +2,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 
 @Injectable()
 export class BankAccountsService {
@@ -11,6 +12,7 @@ export class BankAccountsService {
     createBankAccountDto: CreateBankAccountDto,
     tokenPayloadDto: TokenPayloadDto,
   ) {
+    console.log(createBankAccountDto, tokenPayloadDto.sub);
     try {
       const account = await this.prisma.bank_account.create({
         data: {
@@ -23,9 +25,80 @@ export class BankAccountsService {
       });
 
       return account;
+    } catch (error) {
+      return new HttpException(
+        'Unable to create account: ' + error.message,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async getBankAccounts(tokenPayloadDto: TokenPayloadDto) {
+    const accounts = await this.prisma.bank_account.findMany({
+      where: {
+        user_id: tokenPayloadDto.sub,
+      },
+    });
+
+    return accounts;
+  }
+
+  async updateBankAccount(
+    id: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+    tokenPayloadDto: TokenPayloadDto,
+  ) {
+    const bankAccount = await this.prisma.bank_account.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!bankAccount) {
+      return new HttpException('Bank account not found', HttpStatus.NOT_FOUND);
+    }
+
+    if (bankAccount.user_id !== tokenPayloadDto.sub) {
+      return new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+
+    try {
+      const updated = await this.prisma.bank_account.update({
+        where: {
+          id,
+        },
+        data: {
+          bank_name: updateBankAccountDto.bank_name || bankAccount.bank_name,
+          account_type:
+            updateBankAccountDto.account_type || bankAccount.account_type,
+          balance: updateBankAccountDto.balance || bankAccount.balance,
+          currency: updateBankAccountDto.currency || bankAccount.currency,
+        },
+      });
+
+      return updated;
     } catch {
       return new HttpException(
-        'Unable to create account',
+        'Nao foi possivel adicionar a conta bancaria',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async deleteBankAccount(id: string) {
+    try {
+      await this.prisma.bank_account.delete({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        message: 'Bank account deleted',
+      };
+    } catch (error) {
+      return new HttpException(
+        'Erro ao deletar conta bancaria: ' + error.message,
         HttpStatus.BAD_REQUEST,
       );
     }
